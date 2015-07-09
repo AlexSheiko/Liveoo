04ac299dae18cde6a3df05b5a351366678b3a03d
Aleksey Sheyko <aleksey_sheyko@hotmail.com>
2015-07-10 02:13:15 +0300

    Center actionbar title

--- app/src/main/java/com/liveooapp/liveoo/MainActivity.java
+++ app/src/main/java/com/liveooapp/liveoo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.liveooapp.liveoo;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView actionBar = (TextView) getLayoutInflater().inflate(R.layout.actionbar, null);
+        actionBar.setText(getString(R.string.activity_main_title));
+
+        ActionBar.LayoutParams layout = new ActionBar.LayoutParams(
+                ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(actionBar, layout);
     }
 
     public void openCategory(View view) {
