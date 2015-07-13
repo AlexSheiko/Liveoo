5eb66fe44a2078465cf79ad2e6aaffb40efecfb1
Aleksey Sheyko <aleksey_sheyko@hotmail.com>
2015-07-13 22:21:02 +0300

    Create payment result activity

--- app/src/main/java/com/liveooapp/liveoo/TicketPayActivity.java
+++ app/src/main/java/com/liveooapp/liveoo/TicketPayActivity.java
@@ -1,7 +1,9 @@
 package com.liveooapp.liveoo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class TicketPayActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class TicketPayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_pay);
+    }
+
+    public void makePayment(View view) {
+        startActivity(new Intent(this, TicketResultActivity.class));
     }
 }
