9a0315724f8ee0111ae745849ff370cb97935369
Aleksey Sheyko <aleksey_sheyko@hotmail.com>
2015-07-14 00:41:35 +0300

    Set different sample images to tickets list

--- app/src/main/java/com/liveooapp/liveoo/adapters/TicketAdapter.java
+++ app/src/main/java/com/liveooapp/liveoo/adapters/TicketAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.liveooapp.liveoo.R;
@@ -34,6 +35,14 @@ public class TicketAdapter extends ArrayAdapter<Ticket> {
         ((TextView) rootView.findViewById(R.id.address)).setText(ticket.getAddress());
         ((TextView) rootView.findViewById(R.id.datetime)).setText(ticket.getDateTime());
         ((TextView) rootView.findViewById(R.id.location)).setText(ticket.getLocation());
+
+        if (position == 1) {
+            ((ImageView) rootView.findViewById(R.id.icon)).setImageDrawable(
+                    getContext().getResources().getDrawable(R.drawable.ic_ticket_2));
+        } else if (position == 2) {
+            ((ImageView) rootView.findViewById(R.id.icon)).setImageDrawable(
+                    getContext().getResources().getDrawable(R.drawable.ic_ticket_3));
+        }
 
         return rootView;
     }
